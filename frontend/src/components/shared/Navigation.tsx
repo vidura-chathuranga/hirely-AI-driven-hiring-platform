27b32a5337b7195b8a74f71512d2b2835b68d68e
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-22 20:31:23 +0530

    implemented basic navigation and hero secion

--- frontend/src/components/shared/Navigation.tsx
+++ frontend/src/components/shared/Navigation.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Navigation = () => {
   return (
     <nav className="flex py-12 justify-between items-center">
-        <h1>Navgation</h1>
+      <div>
+        <Link to={"/"} className="text-4xl font-medium">HirelyAI</Link>
+      </div>
+      <div className="flex justify-center gap-x-8 items-center">
+        <Link to={"/home"}>Home</Link>
+        <div className="flex gap-x-4 items-center">
+          <Link to={"/home"}>Sign In</Link>
+          <Link to={"/home"}>Sign Up</Link>
+        </div>
+      </div>
     </nav>
   );
 };
--- frontend/src/pages/home/components/Hero.tsx
+++ frontend/src/pages/home/components/Hero.tsx
@@ -1,9 +1,11 @@
 
 const Hero = () => {
   return (
-    <section>
-        <div>Hero Section</div>
-    </section>
+    <section className="py-32 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero">
+    <div>
+      <h1 className="text-white">Find your dream job</h1>
+    </div>
+  </section>
   )
 }
 
