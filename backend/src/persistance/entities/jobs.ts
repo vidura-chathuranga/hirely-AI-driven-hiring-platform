da716b6cfd4f59d0b8bbb73568f4c708b5b48a74
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-02-12 20:20:08 +0530

    added default questions

--- backend/src/persistance/entities/jobs.ts
+++ backend/src/persistance/entities/jobs.ts
@@ -15,7 +15,11 @@ const jobSchema = new mongoose.Schema({
   },
   questions: {
     type: [String],
-    default: ["Question 1", "Question 2", "Question 3"],
+    default: [
+      "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+      "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+      "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+    ],
   },
 });
 
