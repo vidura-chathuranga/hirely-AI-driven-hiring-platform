bd102989c9341a1d6923c3c203b5a5d0c015137f
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-02-13 10:49:20 +0530

    remove unwanted mock data from home

--- frontend/src/pages/home/components/JobSection.tsx
+++ frontend/src/pages/home/components/JobSection.tsx
@@ -14,32 +14,17 @@ const JobSection = () => {
   });
 
   if (isLoading) {
-    return <Loader2 className="animate-spin" />;
+    return (
+      <div className="flex justify-center mt-5">
+        <Loader2 className="animate-spin" size={30} />
+      </div>
+    );
   }
 
   if (isError) {
     console.log(error.message);
   }
-  // const jobs = [
-  //   {
-  //     _id: "aadasd7a8787asd",
-  //     title: "Intern - Software Engineer",
-  //     type : "Part Time",
-  //     location : "Remote"
-  //   },
-  //   {
-  //     _id: "aadasd734a8787asd",
-  //     title: "Intern - Software Engineer",
-  //     type : "Part Time",
-  //     location : "Remote"
-  //   },
-  //   {
-  //     _id: "aadasd74546a8787asd",
-  //     title: "Intern - Software Engineer",
-  //     type : "Part Time",
-  //     location : "Remote"
-  //   },
-  // ];
+
   return (
     <section className="py-8">
       <h2>Available Jobs</h2>
