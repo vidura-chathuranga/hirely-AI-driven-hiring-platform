4a5323eb89528f77855402881caf55a314612b0d
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-03-07 14:50:23 +0530

    integrated zpd with create job post admin side

--- frontend/src/pages/admin/createJob/job-create.page.tsx
+++ frontend/src/pages/admin/createJob/job-create.page.tsx
@@ -6,145 +6,191 @@ import { QueryClient, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { Loader2 } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { ZodType, z } from "zod";
+import {zodResolver} from '@hookform/resolvers/zod';
+
+type Inputs = {
+  title: string;
+  description: string;
+  location: string;
+  type: string;
+  q1: string;
+  q2: string;
+  q3: string;
+};
+
+const jobPostSchema: ZodType<Inputs> = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title should have at least 3 characters" }),
+  description: z
+    .string()
+    .min(3, { message: "Description should have at least 3 characters" }),
+  location: z
+    .string()
+    .min(3, { message: "Location should have at least 3 characters" }),
+  type: z
+    .string()
+    .min(3, { message: "Type should have at least 3 characters" }),
+  q1: z
+    .string()
+    .min(5, { message: "Qestion should have at least 5 characters" }),
+  q2: z
+    .string()
+    .min(5, { message: "Qestion should have at least 5 characters" }),
+  q3: z
+    .string()
+    .min(5, { message: "Qestion should have at least 5 characters" }),
+});
 
 const JobCreatePage = () => {
   // toast hook
   const { toast } = useToast();
 
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    type: "",
-    location: "",
-    q1: "",
-    q2: "",
-    q3: "",
+  // get react hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      type: "",
+      location: "",
+      q1: "",
+      q2: "",
+      q3: "",
+    },
+    resolver : zodResolver(jobPostSchema)
   });
+
+  // const [formData, setFormData] = useState({
+  //   title: "",
+  //   description: "",
+  //   type: "",
+  //   location: "",
+  //   q1: "",
+  //   q2: "",
+  //   q3: "",
+  // });
 
   const queryClient = new QueryClient();
   const navigate = useNavigate();
 
-  const { isPending, mutate: createJob } = useMutation({
-    mutationFn: async () => {
-      return axios.post("/api/jobs", formData).then((res) => res.data);
-    },
-    onSuccess: (newJob) => {
-      queryClient.setQueryData(["jobs", newJob._id], newJob);
-      navigate("/admin/jobs");
+  // const { isPending, mutate: createJob } = useMutation({
+  //   mutationFn: async () => {
+  //     return axios.post("/api/jobs", formData).then((res) => res.data);
+  //   },
+  //   onSuccess: (newJob) => {
+  //     queryClient.setQueryData(["jobs", newJob._id], newJob);
+  //     navigate("/admin/jobs");
 
-      // show success notification to the user
-      toast({
-        title: "Job posted Successfully",
-      });
-    },
-    onError: (error) => {
-      // show error to the user
-      toast({
-        title: "oops, There was an error",
-        description: error.message,
-        variant: "destructive",
-      });
-      console.log(error);
-    },
-  });
+  //     // show success notification to the user
+  //     toast({
+  //       title: "Job posted Successfully",
+  //     });
+  //   },
+  //   onError: (error) => {
+  //     // show error to the user
+  //     toast({
+  //       title: "oops, There was an error",
+  //       description: error.message,
+  //       variant: "destructive",
+  //     });
+  //     console.log(error);
+  //   },
+  // });
 
-  const handleChange = (
-    e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (
+  //   e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  // ) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
+  // };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  // const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
 
-    // mutate the tenstack mutation function
-    createJob();
-  };
+  //   // mutate the tenstack mutation function
+  //   createJob();
+  // };
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
     <div>
       <div className="py-8">
         <h2>Create A Job Posting</h2>
       </div>
-      <form className="py-8" onSubmit={handleSubmit}>
+      <form className="py-8" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <h3>Title</h3>
           <Input
+            type="text"
             className="mt-2"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            required
+            {...(register("title"))}
           />
+          <span className="text-red-600">{errors?.title?.message}</span>
         </div>
         <div className="mt-4">
           <h3>Description</h3>
           <Textarea
             className="mt-2"
-            name={"description"}
-            value={formData.description}
-            onChange={handleChange}
-            required
+            {...(register("description"))}
           />
+          <span className="text-red-600">{errors?.description?.message}</span>
         </div>
         <div className="mt-4">
           <h3>Type</h3>
           <Input
             className="mt-2"
-            name="type"
-            value={formData.type}
-            onChange={handleChange}
-            required
+            type="text"
+            {...(register("type"))}
           />
+          <span className="text-red-600">{errors?.type?.message}</span>
         </div>
         <div className="mt-4">
           <h3>Location</h3>
           <Input
             className="mt-2"
-            name="location"
-            value={formData.location}
-            onChange={handleChange}
-            required
+            type="text"
+            {...(register("location"))}
           />
+          <span className="text-red-600">{errors?.location?.message}</span>
         </div>
         <div className="mt-4">
           <h3>Question 1</h3>
           <Textarea
             className="mt-2"
-            name={"q1"}
-            value={formData.q1}
-            onChange={handleChange}
-            required
+            {...(register("q1"))}
           />
+          <span className="text-red-600">{errors?.q1?.message}</span>
         </div>
         <div className="mt-4">
           <h3>Question 2</h3>
           <Textarea
             className="mt-2"
-            name={"q2"}
-            value={formData.q2}
-            onChange={handleChange}
-            required
+            {...(register("q2"))}
           />
+          <span className="text-red-600">{errors?.q2?.message}</span>
         </div>
         <div className="mt-4">
           <h3>Question 3</h3>
           <Textarea
             className="mt-2"
-            name={"q3"}
-            value={formData.q3}
-            onChange={handleChange}
-            required
+            {...(register("q3"))}
           />
+          <span className="text-red-600">{errors?.q3?.message}</span>
         </div>
 
         <Button
           type="submit"
           className="mt-8 bg-card text-card-foreground flex gap-x-3"
-          disabled={isPending}
+          disabled={isSubmitting}
         >
-          {isPending && <Loader2 className="animate-spin" />}
+          {isSubmitting && <Loader2 className="animate-spin" />}
           Submit
         </Button>
       </form>
